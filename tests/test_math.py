0943057d0a9257b50b046e7c92fd1b6d02e81b71
genvarek <genvarek@gmail.com>
2022-07-08 15:49:51 +0600

    Add first pytest practice examples

---
+++ tests/test_math.py
@@ -0,0 +1,30 @@
+import pytest
+
+
+def test_one_plus_one():
+    assert 1+1 == 2
+
+def test_one_plus_two():
+    '''Test should fail'''
+    a = 1
+    b = 2
+    c = 0
+    assert a + b == c
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1/0
+    assert "division by zero" in str(e.value)
+
+products = [
+    (2, 3, 6), # positive integers
+    (1, 23, 23), # identity
+    (0, 1234, 0), # zero
+    (1, -1, -1), # positive by negative
+    (-2, -2, 4), # negative by negative
+    (0.5, 2.0, 1.0) # float
+]
+
+@pytest.mark.parametrize("a, b, product", products)
+def test_multiplictation(a, b, product):
+    assert a * b == product
